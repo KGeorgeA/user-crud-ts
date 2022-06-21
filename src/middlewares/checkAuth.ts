9069be82d7e8a5cf07c306f1f3c4760ff9882917
George Karcivadze <karcivadzega@gmail.com>
2022-06-21 13:24:12 +0300

    added checkAuth middleware

--- src/middlewares/checkAuth.ts
+++ src/middlewares/checkAuth.ts
@@ -1,12 +1,41 @@
 import type { RequestHandler } from 'express';
-import token from 'src/utils/token';
+import { StatusCodes } from 'http-status-codes';
+import CustomError, { createInternalServerError } from 'src/utils/CustomError';
+// import { StatusCodes } from 'http-status-codes';
+// import token from 'src/utils/token';
 
 const checkAuth: RequestHandler = async (req, res, next) => {
   try {
-    const tokenString = (req.headers.authorization || '').replace(/^Bearer /, '');
-    const id = token.verify(tokenString);
+    // const tokenString = (req.headers.authorization || '').replace(/^Bearer /, '');
+    // const { userId } = await token.verify(tokenString, 'accessTokenKey');
+
+    // const user = await db.entities.repo.findByPk(userId);
+
+    // if (!user) {
+    //   res.status(StatusCodes.UNAUTHORIZED).json('User not found');
+    // }
+
+    // req.user = user;
+
+    next();
   } catch (error) {
-    //
+    if (error.name === 'TokenExpiredError') {
+      throw new CustomError<null>({
+        statusCode: StatusCodes.UNAUTHORIZED,
+        message: 'Token is expired',
+        data: null,
+      }, error.message);
+    }
+
+    if (error.name === 'JsonWebTokenError') {
+      throw new CustomError<null>({
+        statusCode: StatusCodes.UNAUTHORIZED,
+        message: 'Token is invalid',
+        data: null,
+      }, error.message);
+    }
+
+    throw createInternalServerError();
   }
 };
 
