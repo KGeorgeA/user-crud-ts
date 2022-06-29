f4fa63b0abb0d42722a4b0ba43bbcb4655052562
George Karcivadze <karcivadzega@gmail.com>
2022-06-29 17:23:42 +0300

    set up type for schema prop

--- src/middlewares/validator.ts
+++ src/middlewares/validator.ts
@@ -3,8 +3,15 @@ import { StatusCodes } from 'http-status-codes';
 import * as yup from 'yup';
 import CustomError from '../utils/CustomError';
 
-// TO-DO: schema should be typed
-const validator = (schema: unknown): Handler => {
+type FieldValidationType = yup.StringSchema | yup.NumberSchema | yup.DateSchema
+type SchemaObject = Record<string, FieldValidationType>
+type SchemaType = {
+  body?: SchemaObject;
+  params?: SchemaObject;
+  query?: SchemaObject;
+}
+
+const validator = (schema: SchemaType): Handler => {
   return async (req, res, next) => {
     try {
       const errors = [];
